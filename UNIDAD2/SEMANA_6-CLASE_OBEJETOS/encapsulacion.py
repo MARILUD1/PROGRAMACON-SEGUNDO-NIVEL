472ca625a850cff0338b8a50295fc3e89870c4d6
Marilud1 <cm.castilloc@uea.edu.ec>
2024-07-05 20:06:36 -0500

    l polimorfismo.-es la capacidad de diferentes objetos de diferentes clases y son tratados como objetos de una clase común, a través de métodos compartidos  funcionan de diferentes maneras en distintos objetos.

--- UNIDAD2/SEMANA_6-CLASE_OBEJETOS/encapsulacion.py
+++ UNIDAD2/SEMANA_6-CLASE_OBEJETOS/encapsulacion.py
@@ -1,52 +1,12 @@
-class Persona: #plantilla para crear el objeto
-    #clase Padre
-    def __init__(self, nombre, apellido, edad,correo,contraseña): #atributos para clase hija
+class Persona:
+    # Clase Padre
+    def __init__(self, nombre, apellido, edad, correo, contraseña):
+        # Atributos para clase hija
         self.nombre = nombre
         self.apellido = apellido
         self.edad = edad
         self.correo = correo
-        self.contraseña=contraseña
-
-
-class Doctor(Persona): #Clase hija
-    def __init__(self, nombre, apellido, edad, correo, contraseña, bisturi):
-        super().__init__(nombre, apellido, edad,correo,contraseña)
-
-        self.__contraseña = contraseña # Encapsulación de la contraseña
-        self.bisturi= bisturi # atributo propio
-
-    def mostrar_informacion(self):
-        return f"Doctor {self.nombre} {self.apellido}, {self.edad} años, correo: {self.correo}, bisturi: {self.bisturi}"
-
-    @property
-    def contraseña (self):
-        return self.__contraseña
-
-    @contraseña.setter
-    def contraseña(self, nueva_contraseña):
-        self.__contraseña = nueva_contraseña
-
-# Crear el objeto de Doctor con atributos heredados y propio
-profesion_doctor = Doctor("Esteban", "Torres", 36, "esteban36@gmail.com", "esDoc36", "opera pacientes")
-
-# Mostrar la información del doctor
-print(profesion_doctor.mostrar_informacion())
-print("Contraseña del doctor:", profesion_doctor.contraseña)
-
-# Cambiar la contraseña
-profesion_doctor.contraseña = "nuevaContraseñaDoc"
-print("Nueva contraseña del doctor:", profesion_doctor.contraseña)
-
-class Chef(Persona): #Clase hija
-    def __init__(self, nombre, apellido, edad, correo, contraseña, sarten):
-        super().__init__(nombre, apellido, edad,correo,contraseña)
-
-     # Encapsulación de la contraseña
         self.__contraseña = contraseña
-        self.sarten = sarten   # atributo propio
-
-    def mostrar_informacion(self):
-        return f"Chef {self.nombre} {self.apellido}, {self.edad} años, correo: {self.correo}, sarten: {self.sarten}"
 
     @property
     def contraseña(self):
@@ -56,13 +16,54 @@ class Chef(Persona): #Clase hija
     def contraseña(self, nueva_contraseña):
         self.__contraseña = nueva_contraseña
 
-# Crear el objeto de Chef con atributos heredados y propio
+
+class Doctor(Persona): # Clase hija
+    def __init__(self, nombre, apellido, edad, correo, contraseña, bisturi):
+        super().__init__(nombre, apellido, edad, correo, contraseña)
+        self.bisturi = bisturi # Atributo propio
+
+    def mostrar_informacion(self):
+        return f"Doctor {self.nombre} {self.apellido}, {self.edad} años, correo: {self.correo}, bisturi: {self.bisturi}"
+
+
+class Chef(Persona): # Clase hija
+    def __init__(self, nombre, apellido, edad, correo, contraseña, sarten):
+        super().__init__(nombre, apellido, edad, correo, contraseña)
+        self.sarten = sarten # Atributo propio
+
+    def mostrar_informacion(self):
+        return f"Chef {self.nombre} {self.apellido}, {self.edad} años, correo: {self.correo}, sarten: {self.sarten}"
+
+
+# Crear el objeto de Doctor con atributos heredados y propio
+profesion_doctor = Doctor("Esteban", "Torres", 36, "esteban36@gmail.com", "esDoc36", "opera pacientes")
+
+# Mostrar la información del doctor
+print(profesion_doctor.mostrar_informacion())
+
+# Solicitar la contraseña  del doctor por consola
+ingresa_contraseña = input("Ingrese la contraseña actual del doctor: ")
+if ingresa_contraseña == profesion_doctor.contraseña:
+
+    nueva_contraseña = input("Ingrese la nueva contraseña del doctor: ")
+
+    print("La contraseña correcta.")
+else:
+    print("Contraseña incorrecta.")
+
+# Crear el objeto Chef con atributos heredados y propio
 profesion_chef = Chef("Lucia", "Rodriguez", 28, "lucia28@gmail.com", "chef1234", "prepara alimentos")
 
 # Mostrar la información del chef
 print(profesion_chef.mostrar_informacion())
-print("Contraseña del chef:", profesion_chef.contraseña)
 
-# Cambiar la contraseña
-profesion_chef.contraseña = "nuevaContraseñaChef"
-print("Nueva contraseña del chef:", profesion_chef.contraseña)
+# Solicitar la contraseña  del chef por consola
+ingresar_contraseña = input("Ingrese la contraseña actual del chef: ")
+if ingresar_contraseña == profesion_chef.contraseña:
+
+
+    print("La contraseña correcta.")
+else:
+    print("Contraseña incorrecta.")
+
+

67cd3236aa5da40e6140e2e7fbdc81f75d305ef0
Marilud1 <cm.castilloc@uea.edu.ec>
2024-07-05 21:58:54 -0500

    l polimorfismo.-es la capacidad de diferentes objetos de diferentes clases y son tratados como objetos de una clase común, a través de métodos compartidos  funcionan de diferentes maneras en distintos objetos.

--- UNIDAD2/SEMANA_6-CLASE_OBEJETOS/encapsulacion.py
+++ UNIDAD2/SEMANA_6-CLASE_OBEJETOS/encapsulacion.py
@@ -34,7 +34,6 @@ class Chef(Persona): # Clase hija
     def mostrar_informacion(self):
         return f"Chef {self.nombre} {self.apellido}, {self.edad} años, correo: {self.correo}, sarten: {self.sarten}"
 
-
 # Crear el objeto de Doctor con atributos heredados y propio
 profesion_doctor = Doctor("Esteban", "Torres", 36, "esteban36@gmail.com", "esDoc36", "opera pacientes")
 
@@ -42,12 +41,11 @@ profesion_doctor = Doctor("Esteban", "Torres", 36, "esteban36@gmail.com", "esDoc
 print(profesion_doctor.mostrar_informacion())
 
 # Solicitar la contraseña  del doctor por consola
+
 ingresa_contraseña = input("Ingrese la contraseña actual del doctor: ")
 if ingresa_contraseña == profesion_doctor.contraseña:
-
-    nueva_contraseña = input("Ingrese la nueva contraseña del doctor: ")
-
     print("La contraseña correcta.")
+
 else:
     print("Contraseña incorrecta.")
 
@@ -56,11 +54,10 @@ profesion_chef = Chef("Lucia", "Rodriguez", 28, "lucia28@gmail.com", "chef1234",
 
 # Mostrar la información del chef
 print(profesion_chef.mostrar_informacion())
-
 # Solicitar la contraseña  del chef por consola
 ingresar_contraseña = input("Ingrese la contraseña actual del chef: ")
-if ingresar_contraseña == profesion_chef.contraseña:
 
+if ingresar_contraseña == profesion_chef.contraseña:
 
     print("La contraseña correcta.")
 else:

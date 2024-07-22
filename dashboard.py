c69947c329a171eab71bf3f711487158ac87add1
Marilud1 <cm.castilloc@uea.edu.ec>
2024-07-21 22:52:41 -0500

    dashboard se compone de objetos que encapsulan datos, comportamiento y presentación

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,7 @@ import os
 
 
 def mostrar_codigo(ruta_script):
-    # Asegúrate de que la ruta al script es absoluta
+    # Asegúrate de que la ruta al script es absoluta. MOstrar en codigo que contiene el arhivo
     ruta_script_absoluta = os.path.abspath(ruta_script)
     try:
         with open(ruta_script_absoluta, 'r') as archivo:
@@ -18,9 +18,9 @@ def mostrar_menu():
     # Define la ruta base donde se encuentra el dashboard.py
     ruta_base = os.path.dirname(__file__)
 
-    opciones = {
+    opciones = { # los archivos con su respectivo codigo
 
-        '1': 'UNIDAD 1/SEMANA2/1.1 abstraccion.py',
+        '1': 'UNIDAD 1/SEMANA2/1.1 abstraccion.py', # Ruta del Archivos con el respectivo codigo en orden jerarjico.
         '2': 'UNIDAD 1/SEMANA2/1.2 encapsulacion.py',
         '3': 'UNIDAD 1/SEMANA2/1.3 polimorfismo.py',
         '4': 'UNIDAD 1/SEMANA2/1.4 tecnica_herencia.py',
@@ -34,26 +34,24 @@ def mostrar_menu():
         '12': 'UNIDAD2/SEMANA_6/1.2 metodo_polimorfismo.py',
         '13': 'UNIDAD2/SEMANA7/1.1 constructores y destructores.py',
 
-
-        # Agrega aquí el resto de las rutas de los scripts
     }
 
     while True:
-        print("\nMenu Principal - Dashboard")
-        # Imprime las opciones del menú
-        for key in opciones:
-            print(f"{key} - {opciones[key]}")
+        print("\nMenu Principal - Dashboard")# Inicia un bucle infinito para mostrar el menú repetidamente hasta que el usuario decida salir.
+        # Imprime las opciones del menú principal o padre
+        for key in opciones:#Recorre las claves del diccionario 'opciones'
+            print(f"{key} - {opciones[key]}")#Muestra cada opción del menú en el formato con su identificador y su valor
         print("0 - Salir")
 
-        eleccion = input("Elige un script para ver su código o '0' para salir: ")
+        eleccion = input("Elige un script para ver su código o '0' para salir: ")# ingresar por consola para consultar
         if eleccion == '0':
             break
         elif eleccion in opciones:
-            # Asegura que el path sea absoluto
+            # Asegura que el path sea absoluto Es la ubicación de un archivo o directorio
             ruta_script = os.path.join(ruta_base, opciones[eleccion])
-            mostrar_codigo(ruta_script)
+            mostrar_codigo(ruta_script) # en caso de ser correcto
         else:
-            print("Opción no válida. Por favor, intenta de nuevo.")
+            print("Opción no válida. Por favor, intenta de nuevo.")# Cuando el archivo no existe
 
 
 # Ejecutar el dashboard
